3bbe5c797ce3868dee9fe08517545b1ba28e2f46
AliyanJabbar <jabbaraliyan805@gmail.com>
2024-03-04 06:43:07 +0500

    first commit

---
+++ Day1/Q2/app.ts
@@ -0,0 +1,4 @@
+export {}  // I give it a seperate identity to solve {Cannot redclare block scoped variable}
+let name: string ="Hassan Sultan"  // Variables keep data you can use later. 
+console.log(`Hello ${name}, would you like to learn some programming today? ` )  // here (``) backtick sign is used to define template literals
+// The dollar sign followed by curly braces ${} is used to evaluate and embed expressions dynamically in template literals
---
+++ Day2/Q2/app.ts
@@ -0,0 +1,4 @@
+export {}
+let famous_person: string = "Imran Khan"
+let message: string= "Absolutely Not"
+console.log(`Once ${famous_person} said to an American reporter, "${message}"`)
