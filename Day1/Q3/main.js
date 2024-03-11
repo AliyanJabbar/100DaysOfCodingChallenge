e3294dee28bea4db49f826ab34b90000346bde08
AliyanJabbar <jabbaraliyan805@gmail.com>
2024-03-11 20:37:04 +0500

    fifth commit

--- Day1/Q3/main.js
+++ Day1/Q3/main.js
@@ -3,5 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var name = "Aliyan Jabbar";
 console.log(name.toLowerCase()); // Shows the name in all small letters
 console.log(name.toUpperCase()); // Shows the name in ALL BIG LETTERS
-console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
-// Shows the name with the First Letter Big
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()); // Shows the name with the First Letter Big
--- Day3/Q9/app.js
+++ Day3/Q9/app.js
@@ -1,2 +1,2 @@
-var favorite_Number = "11"; // This saves our favorite number
-console.log("Hello friends! do you know my favorite number is \"".concat(favorite_Number, "\"")); // This shows the message with your favorite number
+var favorite_Number = 11; // This saves our favorite number
+console.log("Hello friends! do you know my favorite number is \"".concat(favorite_Number, "\"")); // This shows the message with our favorite number
